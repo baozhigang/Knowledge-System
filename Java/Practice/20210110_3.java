20e29cc267e2b48bf1da7ad41da8d7d1abb9f8a4
“baozhigang” <646637969@qq.com>
2021-01-10 12:15:40 +0800

    Update

---
+++ Java/Practice/20210110_3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class DungeonTest {
+    public static void main(String[] args) {
+        DungeonGame d = new DungeonGame();
+        System.out.println(d.getX() + d.getY() + d.getZ());
+
+        try {
+            FileOutputStream fos = new FileOutputStream("dg.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("dg.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            d = (DungeonGame) ois.readObject();
+            ois.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(d.getX() + d.getY() + d.getZ());
+    }
+}
