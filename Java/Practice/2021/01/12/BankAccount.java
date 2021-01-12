6e7f701e7cf2c86a3a267a4b325224b09c19dc88
“baozhigang” <646637969@qq.com>
2021-01-12 22:01:30 +0800

    Update

---
+++ Java/Practice/2021/01/12/BankAccount.java
@@ -0,0 +1,12 @@
+class BankAccount {
+    private int balance = 100;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
+
