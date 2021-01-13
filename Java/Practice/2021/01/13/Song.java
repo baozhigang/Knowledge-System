f5ccbfc514eb0163fed9e90c94c6c0252a80d46c
“baozhigang” <646637969@qq.com>
2021-01-13 22:11:00 +0800

    Update

---
+++ Java/Practice/2021/01/13/Song.java
@@ -0,0 +1,40 @@
+class Song implements Comparable<Song> {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    public Song(String title, String artist, String rating, String bpm) {
+        this.title = title;
+        this.artist = artist;
+        this.rating = rating;
+        this.bpm = bpm;
+    }
+
+    public int compareTo(Song s) {
+        return title.compareTo(s.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
