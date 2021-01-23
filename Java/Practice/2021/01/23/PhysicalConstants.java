cdb19002e65bdec7fae94480830d89227b52099f
“baozhigang” <646637969@qq.com>
2021-01-23 12:29:31 +0800

    Update

---
+++ Java/Practice/2021/01/23/PhysicalConstants.java
@@ -0,0 +1,9 @@
+public class PhysicalConstants {
+    private PhysicalConstants() {
+
+    }
+    
+    public static final double AVO_NUMBER = 6.002_140_857e23;
+    public static final double BOL_CONST = 1.380_648_52e23;
+    public static final double ELECTRON_MASS = 9.109_383_56e31;
+}
