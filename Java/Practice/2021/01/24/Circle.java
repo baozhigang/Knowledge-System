cbd980e3b791ba1f014bfdfce7dd225c801e540e
“baozhigang” <646637969@qq.com>
2021-01-24 10:05:39 +0800

    Update

---
+++ Java/Practice/2021/01/24/Circle.java
@@ -0,0 +1,11 @@
+public class Circle extends Figure {
+    final double radius;
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * (radius * radius);
+    }
+}
