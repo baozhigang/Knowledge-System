c3d153e836ba860b89466366cdd630ccf487a1a5
“baozhigang” <646637969@qq.com>
2021-01-25 14:14:23 +0800

    Update

---
+++ Java/Practice/2021/01/25/Stack.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Stack<E> {
+    private E[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        E result = elements[size--];
+        elements[size] = null;
+        return result;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2*size + 1);
+    }
+}
