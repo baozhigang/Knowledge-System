0c5d6273ccc22cf527b76fdfc527ec7bd2101a2d
“baozhigang” <646637969@qq.com>
2021-01-28 22:44:37 +0800

    Update

---
+++ Java/Practice/2021/01/28/Operation.java
@@ -0,0 +1,24 @@
+public enum Operation {
+    PLUS {
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS {
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TOMES {
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE {
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    public abstract double apply(double x, double y);
+}
