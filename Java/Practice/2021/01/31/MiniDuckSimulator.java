4a5081682e1b0625a3291e77d59096bbbb54a544
“baozhigang” <646637969@qq.com>
2021-01-31 16:06:33 +0800

    Update

---
+++ Java/Practice/2021/01/31/MiniDuckSimulator.java
@@ -0,0 +1,7 @@
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        MallardDuck mallardDuck = new MallardDuck();
+        mallardDuck.performFly();
+        mallardDuck.performQuack();
+    }
+}
