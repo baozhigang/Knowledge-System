09c90547fed57e68cf56b1ee8d25bc8d30b20131
“baozhigang” <646637969@qq.com>
2021-02-04 23:56:28 +0800

    Update

---
+++ Java/Design-Pattern/观察者模式/Demo2/RegNotificationObserver.java
@@ -0,0 +1,7 @@
+public class RegNotificationObserver implements RegObserver {
+    private NotificationService notificationService;
+    @Override
+    public void handleRegSuccess(long userId) {
+        notificationService.sendInboxMessage(userId, "Welcome...");
+    }
+}
