c82e5a3e4b6ac1bc3516fd8bb69c3ac4d2d16220
“baozhigang” <646637969@qq.com>
2021-02-04 22:13:37 +0800

    Update

---
+++ Java/Design-Pattern/观察者模式/Demo1/WeatherStation.java
@@ -0,0 +1,8 @@
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(80, 65, 30.4f);
+    }
+}
