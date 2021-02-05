348fd3c8722d4b65107bd7334e99fc4477bf2ed5
“baozhigang” <646637969@qq.com>
2021-02-05 21:53:43 +0800

    Update

---
+++ Java/Design-Pattern/装饰者模式/Demo1/StarbuzzCoffee.java
@@ -0,0 +1,12 @@
+public class StarbuzzCoffee {
+    public static void main(String[] args) {
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        Beverage beverage2 = new HouseBlend();
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Soy(beverage2);
+        beverage2 = new Whip(beverage2);
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+    }
+}
