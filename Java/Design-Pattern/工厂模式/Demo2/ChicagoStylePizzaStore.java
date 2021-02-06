6248cd7807e7262f0afa79a75f733329d2f38ba2
“baozhigang” <646637969@qq.com>
2021-02-06 10:35:42 +0800

    Update

---
+++ Java/Design-Pattern/工厂模式/Demo2/ChicagoStylePizzaStore.java
@@ -0,0 +1,14 @@
+public class ChicagoStylePizzaStore extends PizzaStore {
+
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+        if (type.equals("cheese")) {
+            pizza = new ChicagoStyleCheesePizza();
+        } else if (type.equals("clam")) {
+            pizza = new ChicagoStyleClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new ChicagoStyleVeggiePizza();
+        }
+        return pizza;
+    }
+}
