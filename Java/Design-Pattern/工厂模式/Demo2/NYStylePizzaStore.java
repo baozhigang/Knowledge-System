ff8cf6fe705c2ea634532ab4a65c09aa4ae764fc
“baozhigang” <646637969@qq.com>
2021-02-06 10:38:07 +0800

    Update

---
+++ Java/Design-Pattern/工厂模式/Demo2/NYStylePizzaStore.java
@@ -0,0 +1,13 @@
+public class NYStylePizzaStore extends PizzaStore {
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+        if (type.equals("cheese")) {
+            pizza = new NYStyleCheesePizza();
+        } else if (type.equals("clam")) {
+            pizza = new NYStyleClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new NYStyleVeggiePizza();
+        }
+        return pizza;
+    }
+}
