3412c932b5c886e3360aab15a7e8d72223f166d8
“baozhigang” <646637969@qq.com>
2021-02-06 15:18:31 +0800

    Update

---
+++ Java/Design-Pattern/单例模式/Demo1/Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton {
+    private static Singleton uniqueInstance;
+    private Singleton() {
+
+    }
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
