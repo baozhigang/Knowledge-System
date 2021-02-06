dc1497ee75cc25fbcd664d4ddf3fb9ab5620ba3c
“baozhigang” <646637969@qq.com>
2021-02-06 09:40:55 +0800

    Update

---
+++ Java/Design-Pattern/工厂模式/Demo1/SimplePizzaFactory.java
@@ -0,0 +1,13 @@
+public class SimplePizzaFactory {
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza();
+        }
+        return pizza;
+    }
+}
