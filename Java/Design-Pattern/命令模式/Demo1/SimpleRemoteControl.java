e883a2b5320944f1fb27cd07d676f3a5ae0edf6b
“baozhigang” <646637969@qq.com>
2021-02-08 09:40:02 +0800

    Update

---
+++ Java/Design-Pattern/命令模式/Demo1/SimpleRemoteControl.java
@@ -0,0 +1,13 @@
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl() {}
+
+    public void setCommand(Command command) {
+        this.slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
