3416b120c9f26fbd001b06411d2f7a07340c09a7
“baozhigang” <646637969@qq.com>
2021-02-13 21:26:56 +0800

    Update

--- Java/Design-Pattern/命令模式/Demo1/GarageDoorOpenCommand.java
+++ Java/Design-Pattern/命令模式/Demo1/GarageDoorOpenCommand.java
@@ -1,8 +1,10 @@
 public class GarageDoorOpenCommand implements Command {
+    public GarageDoorOpenCommand(Command command) {
 
+    }
 
     @Override
     public void execute() {
-        
+
     }
 }
