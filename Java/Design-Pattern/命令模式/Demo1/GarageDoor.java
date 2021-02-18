a919dcaae0f79ca42c43781f0156294fbacb8f4b
“baozhigang” <646637969@qq.com>
2021-02-18 22:02:35 +0800

    Update

---
+++ Java/Design-Pattern/命令模式/Demo1/GarageDoor.java
@@ -0,0 +1,8 @@
+public class GarageDoor {
+    public void open() {
+        System.out.println("Door is open");
+    }
+    public void close() {
+        System.out.println("Door is close");
+    }
+}
