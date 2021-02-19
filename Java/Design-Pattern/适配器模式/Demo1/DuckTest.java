85889b27bab3076e646cb30a063ef63e83621dc5
“baozhigang” <646637969@qq.com>
2021-02-19 22:29:31 +0800

    Update

---
+++ Java/Design-Pattern/适配器模式/Demo1/DuckTest.java
@@ -0,0 +1,22 @@
+public class DuckTest {
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck();
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("The Turkey says...");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\n The Duck says...");
+        testDuck(duck);
+
+        System.out.println("\n The TurkeyAdapter says...");
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
