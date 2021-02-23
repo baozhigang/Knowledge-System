c6e13f3803274421bfc14b4f2ec4f8a9eabc0387
“baozhigang” <646637969@qq.com>
2021-02-23 23:10:00 +0800

    Update

---
+++ Java/Design-Pattern/模板方法模式/Demo1/Coffee.java
@@ -0,0 +1,8 @@
+public class Coffee extends CaffeineBeverage {
+    public void brew() {
+        System.out.println("Coffee");
+    }
+    public void addCondiments() {
+        System.out.println("Add Milk");
+    }
+}
