2653cd0210b11db1287e4c26539868dd8943669b
“baozhigang” <646637969@qq.com>
2021-02-24 23:16:51 +0800

    Update

---
+++ Java/Design-Pattern/迭代器模式/Demo1/DinerMenuIterator.java
@@ -0,0 +1,22 @@
+public class DinerMenuIterator implements Iterator {
+    MenuItem[] menuItems;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public Object next() {
+        MenuItem menuItem = menuItems[position];
+        position++;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        if (position >= menuItems.length || menuItems[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
