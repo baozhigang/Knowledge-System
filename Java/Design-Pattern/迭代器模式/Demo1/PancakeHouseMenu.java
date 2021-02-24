b844688901a3be4ac45ca2a60c1f4177a6d66556
“baozhigang” <646637969@qq.com>
2021-02-24 22:34:45 +0800

    Update

---
+++ Java/Design-Pattern/迭代器模式/Demo1/PancakeHouseMenu.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+    ArrayList menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList();
+        addItem("a", "aaa", true, 2.99);
+        addItem("b", "bbb", false, 2.99);
+        addItem("c", "ccc", true, 3.49);
+        addItem("d", "ddd", true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList getMenuItems() {
+        return menuItems;
+    }
+}
