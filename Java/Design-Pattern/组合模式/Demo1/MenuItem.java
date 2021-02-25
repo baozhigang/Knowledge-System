4b773d385270374e89a7608df1543522d10224ad
“baozhigang” <646637969@qq.com>
2021-02-25 22:56:58 +0800

    Update

---
+++ Java/Design-Pattern/组合模式/Demo1/MenuItem.java
@@ -0,0 +1,42 @@
+public class MenuItem extends MenuComponent {
+    String name;
+    String description;
+    double price;
+    boolean vegetarian;
+
+    public MenuItem(String name, String description, double price, boolean vegetarian) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vegetarian = vegetarian;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void print() {
+        System.out.print(" " + getName());
+        if (isVegetarian()) {
+            System.out.print("(v)");
+        }
+        System.out.println("," + getPrice());
+        System.out.println(" --" + getDescription());
+    }
+}
