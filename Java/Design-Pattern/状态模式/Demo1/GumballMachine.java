a3b3dadf4694f415ebd899a0ca7ab53bb09265e2
“baozhigang” <646637969@qq.com>
2021-02-28 19:48:37 +0800

    Update

---
+++ Java/Design-Pattern/状态模式/Demo1/GumballMachine.java
@@ -0,0 +1,20 @@
+public class GumballMachine {
+    final static int SOLD_OUT = 0;
+    final static int NO_QUARTER = 1;
+    final static int HAS_QUARTER = 2;
+    final static int SOLD = 3;
+
+    int state = SOLD_OUT;
+    int count = 0;
+
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) {
+            state = NO_QUARTER;
+        }
+    }
+
+    public void insertQuarter() {
+        
+    }
+}
