f5bf2051de9d6ccaf3d1e7ea640d48c6fc8e3160
“baozhigang” <646637969@qq.com>
2021-03-01 23:19:57 +0800

    Update

---
+++ Java/Design-Pattern/状态模式/Demo1/HasQuarterState.java
@@ -0,0 +1,25 @@
+public class HasQuarterState implements State {
+    GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void insertQuarter() {
+        System.out.println("You can not insert another quarter");
+    }
+
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    public void turnCrank() {
+        System.out.println("You turned");
+        gumballMachine.setState(gumballMachine.getSoldOutState());
+    }
+
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+}
