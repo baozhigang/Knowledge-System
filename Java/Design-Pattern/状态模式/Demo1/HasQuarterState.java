9ab139f2d4bfb4fd8344fa5754e8275ec334f6a8
“baozhigang” <646637969@qq.com>
2021-03-02 22:32:41 +0800

    Update

--- Java/Design-Pattern/状态模式/Demo1/HasQuarterState.java
+++ Java/Design-Pattern/状态模式/Demo1/HasQuarterState.java
@@ -1,4 +1,7 @@
+import java.util.Random;
+
 public class HasQuarterState implements State {
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
 
     public HasQuarterState(GumballMachine gumballMachine) {
@@ -16,7 +19,12 @@ public class HasQuarterState implements State {
 
     public void turnCrank() {
         System.out.println("You turned");
-        gumballMachine.setState(gumballMachine.getSoldOutState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        }
     }
 
     public void dispense() {
