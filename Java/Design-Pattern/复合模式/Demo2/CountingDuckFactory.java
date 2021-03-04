5964addfeeea53f1483015286eda930f01321c64
“baozhigang” <646637969@qq.com>
2021-03-04 22:32:36 +0800

    Update

---
+++ Java/Design-Pattern/复合模式/Demo2/CountingDuckFactory.java
@@ -0,0 +1,14 @@
+public class CountingDuckFactory extends AbstractDuckFactory {
+    public Quackable createMallardDuck() {
+        return new QuackCounter(new MallardDuck());
+    }
+    public Quackable createRedHeadDuck() {
+        return new QuackCounter(new RedHeadDuck());
+    }
+    public Quackable createDuckCall() {
+        return new QuackCounter(new DuckCall());
+    }
+    public Quackable createRubberDuck() {
+        return new QuackCounter(new RubberDuck());
+    }
+}
