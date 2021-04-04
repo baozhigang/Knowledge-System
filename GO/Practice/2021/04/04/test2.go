0d213ad5af93f081a9ffc6b62f8b8ddd85172df6
“baozhigang” <646637969@qq.com>
2021-04-04 15:43:08 +0800

    Update

---
+++ GO/Practice/2021/04/04/test2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var quantity int = 4;
+    var length,width float64 = 1.4,1.2
+    var customerName string = "Mr.Bao" 
+
+    fmt.Println(quantity)
+    fmt.Println(length)
+    fmt.Println(width)
+    fmt.Println(customerName)
+}
