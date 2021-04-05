86fa826e2e3d808bbf44f2dc0744798ac70c375e
“baozhigang” <646637969@qq.com>
2021-04-05 11:53:31 +0800

    Update

---
+++ GO/Practice/2021/04/05/pass_fail.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	fmt.Println("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	fmt.Println(input)
+}
