760858a4a62798ab49cd3094c392cec3a1629e25
“baozhigang” <646637969@qq.com>
2021-04-06 22:56:18 +0800

    Update

---
+++ GO/Practice/2021/04/06/string_int.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+
+	if grade > 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+
+	fmt.Println(status)
+}
