47884031af73fab7ac2a007639b4e1fe22207c35
“baozhigang” <646637969@qq.com>
2021-04-06 23:40:23 +0800

    Update

---
+++ GO/Practice/2021/04/06/random.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println(target)
+}
