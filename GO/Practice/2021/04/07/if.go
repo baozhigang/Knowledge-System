ef8553a5ed1d9d5b1536caf28b1a680ce4c76a5d
“baozhigang” <646637969@qq.com>
2021-04-07 22:26:02 +0800

    Update

---
+++ GO/Practice/2021/04/07/if.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	if 1==1 {
+		fmt.Println("Yes, you are right")
+	}  
+	if 1>2 {
+		fmt.Println("No, wrong!")
+	} 
+	if 1<2 {
+		fmt.Println("right")
+	}
+}
+
