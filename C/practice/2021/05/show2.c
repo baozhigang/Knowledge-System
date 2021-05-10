896ab359bb8f20a231fb1f1c4da10db2e6e25e82
“baozhigang” <646637969@qq.com>
2021-05-10 23:24:40 +0800

    Update

---
+++ C/practice/2021/05/show2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i=0; i<len; i++) {
+        printf(" %.2x", start[i]);
+        printf("\n");
+    }
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+int main()
+{
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+
+    show_bytes(valp, 1);
+    show_bytes(valp, 2);
+    show_bytes(valp, 3);
+    
+    return 0;
+}
