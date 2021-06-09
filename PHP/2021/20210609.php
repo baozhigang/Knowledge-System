16d4b9ea34442b6d6040a7c70aedd276ec935378
“baozhigang” <646637969@qq.com>
2021-06-09 22:27:55 +0800

    Update

---
+++ PHP/2021/20210609.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$today = getdate();
+print_r($today);
+
+/*
+Array
+(
+    [seconds] => 43
+    [minutes] => 23
+    [hours] => 14
+    [mday] => 9
+    [wday] => 3
+    [mon] => 6
+    [year] => 2021
+    [yday] => 159
+    [weekday] => Wednesday
+    [month] => June
+    [0] => 1623248623
+)
+*/
