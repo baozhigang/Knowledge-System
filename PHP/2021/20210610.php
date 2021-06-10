16c5b3e3a7c957971f0529ae3549a27f38b8a582
“baozhigang” <646637969@qq.com>
2021-06-10 22:25:10 +0800

    Update

---
+++ PHP/2021/20210610.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 常见的数组函数
+ */
+
+array_count_values(); // 统计数组中的值出现的次数
+array_filter(); // 使用回调函数过滤数组的元素
+array_flip(); // 交换数组中的键和值
+array_intersect(); // 计算数组的交集
+array_map(); // 为数组的每个元素应用回调函数
+array_multisort(); // 对多个数组或多维数组进行排序
