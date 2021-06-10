245fb3ca9f8e597d3476ac20edbf202319272a90
“baozhigang” <646637969@qq.com>
2021-06-10 23:19:57 +0800

    Update

---
+++ PHP/2021/20210610_2.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 常见的字符串函数
+ */
+
+addslashes();  // 使用反斜线引用字符串
+chr(); // 返回相对应于 ascii 所指定的单个字符
+chunk_split(); // 将字符串分割成小块
+md5(); // 计算字符串的 MD5 散列值
+str_repeat(); // 重复一个字符串
+str_replace(); // 子字符串替换
+str_shuffle(); // 随机打乱一个字符串
+str_split(); // 将字符串转换为数组
+strlen(); // 获取字符串长度
+strrev(); // 反转字符串 
+strtolower(); // 将字符串转化为小写
+strtoupper(); // 将字符串转化为大写
+substr_count(); // 计算字串出现的次
+substr(); // 返回字符串的子串数
+wordwrap(); // 打断字符串为指定数量的字串
