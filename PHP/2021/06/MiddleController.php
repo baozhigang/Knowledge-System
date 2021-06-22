8fb1c2fa2bfc512bc7380fe29cdef309fe08aec6
“baozhigang” <646637969@qq.com>
2021-06-22 23:50:29 +0800

    Update

---
+++ PHP/2021/06/MiddleController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+use Hyperf\HttpServer\Annotation\AutoController;
+
+/**
+ * @AutoController()
+ * @Middleware(FooMiddleware::class)
+ */
+class MiddleController extends AbstractController
+{
+    public function index()
+    {
+        return 'Hello Hyperf';
+    }
+
+}
