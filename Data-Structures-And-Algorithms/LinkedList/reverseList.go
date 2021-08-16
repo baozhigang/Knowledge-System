6f9fe650af62bdb1a2def15c9121375dad2f985d
“baozhigang” <646637969@qq.com>
2021-08-16 15:17:46 +0800

    Update

---
+++ Data-Structures-And-Algorithms/LinkedList/reverseList.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
