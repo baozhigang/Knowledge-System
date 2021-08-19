3fe061eed2989f4ecb5b68c56f381c7d7059ce28
“baozhigang” <646637969@qq.com>
2021-08-20 00:12:25 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Tree/leetCode-235-lowestCommonAncestor.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	curr := root
+	for {
+		if p.Val < curr.Val && q.Val < curr.Val {
+			curr = curr.Left
+		} else if p.Val > curr.Val && q.Val > curr.Val {
+			curr = curr.Right
+		} else {
+			return curr
+		}
+	}
+}
