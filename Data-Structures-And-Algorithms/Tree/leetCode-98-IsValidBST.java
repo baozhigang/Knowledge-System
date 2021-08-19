70de521b56b437c4361db4f0683b738754289d7a
“baozhigang” <646637969@qq.com>
2021-08-19 23:14:54 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Tree/leetCode-98-IsValidBST.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class IsValidBST {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long lower, long upper) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= lower || root.val >= upper) {
+            return false;
+        }
+        return isValidBST(root.left, lower, root.val) &&
+                isValidBST(root.right, root.val, upper);
+    }
+}
