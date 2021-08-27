ca4d209b67e4fcfa6dca57e91ce853aa664519ab
“baozhigang” <646637969@qq.com>
2021-08-27 11:32:48 +0800

    Update

---
+++ Data-Structures-And-Algorithms/BinarySearch/leetCode-704-bsearch.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
