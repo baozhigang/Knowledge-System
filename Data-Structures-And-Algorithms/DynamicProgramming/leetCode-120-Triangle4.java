383c8cae8075ddbe614cb26ae5e28d01dfc4976d
“baozhigang” <646637969@qq.com>
2021-09-04 13:20:11 +0800

    Update

---
+++ Data-Structures-And-Algorithms/DynamicProgramming/leetCode-120-Triangle4.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Triangle4 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n+1];
+        for (int i = n-1; i >=0 ; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        item.add(2);
+        triangle.add(item);
+
+        ArrayList<Integer> item2 = new ArrayList<Integer>();
+        item2.add(3);
+        item2.add(4);
+        triangle.add(item2);
+
+        ArrayList<Integer> item3 = new ArrayList<Integer>();
+        item3.add(6);
+        item3.add(5);
+        item3.add(7);
+        triangle.add(item3);
+
+        ArrayList<Integer> item4 = new ArrayList<Integer>();
+        item4.add(4);
+        item4.add(1);
+        item4.add(8);
+        item4.add(3);
+        triangle.add(item4);
+
+        System.out.println(triangle);
+
+        Triangle4 triangle4 = new Triangle4();
+        int ret = triangle4.minimumTotal(triangle);
+        System.out.println(ret);
+    }
+}
