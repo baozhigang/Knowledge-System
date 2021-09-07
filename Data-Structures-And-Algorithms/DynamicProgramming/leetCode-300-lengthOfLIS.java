5d7ae01db102c5373b99d2cd9c8362051f219bea
“baozhigang” <646637969@qq.com>
2021-09-08 00:20:44 +0800

    Update

---
+++ Data-Structures-And-Algorithms/DynamicProgramming/leetCode-300-lengthOfLIS.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int maxans = 1;
+        for (int i=0; i<nums.length; i++) {
+            dp[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            maxans = Math.max(dp[i], maxans);
+        }
+        return maxans;
+    }
+}
