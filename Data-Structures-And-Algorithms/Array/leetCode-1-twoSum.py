e2dc14de85f74d9fee2b0fce4dae1296488e9f27
“baozhigang” <646637969@qq.com>
2021-09-30 00:13:33 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Array/leetCode-1-twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+            else:
+                d[n] = i
+
