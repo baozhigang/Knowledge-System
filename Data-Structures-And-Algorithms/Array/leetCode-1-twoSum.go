bb80b3662d0532178bc8673d44623bd62108c445
“baozhigang” <646637969@qq.com>
2021-09-29 23:46:55 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Array/leetCode-1-twoSum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+	table := map[int]int{}
+	for i, x := range nums {
+		if p, ok := table[target-x]; ok {
+			return []int{p, i}
+		}
+		table[x] = i
+	}
+	return nil
+}
+
