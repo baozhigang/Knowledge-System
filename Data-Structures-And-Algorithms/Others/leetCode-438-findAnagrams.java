0470024ff6a369c19f1023f70dfea18c5483c8cc
“baozhigang” <646637969@qq.com>
2021-10-19 01:08:24 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Others/leetCode-438-findAnagrams.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int n = s.length(), m = p.length();
+        List<Integer> res = new ArrayList<>();
+        if (n < m) return res;
+
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+
+        for (int i = 0; i < m; i++) {
+            pCnt[p.charAt(i) - 'a']++;
+        }
+
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            int curRight = s.charAt(right) - 'a';
+            sCnt[curRight]++;
+            while(sCnt[curRight] > pCnt[curRight]){
+                int curLeft = s.charAt(left) - 'a';
+                sCnt[curLeft]--;
+                left++;
+            }
+
+            if(right - left + 1 == m){
+                res.add(left);
+            }
+        }
+        return res;
+    }
+}
