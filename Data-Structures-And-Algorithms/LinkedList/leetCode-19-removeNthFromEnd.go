0a6ded2f3003fad3d5d74c63ba5c6a6fa1876f03
“baozhigang” <646637969@qq.com>
2021-10-23 17:12:28 +0800

    Update

---
+++ Data-Structures-And-Algorithms/LinkedList/leetCode-19-removeNthFromEnd.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	fast, slow := head, dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for ; fast != nil; fast = fast.Next {
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
+
