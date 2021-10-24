cbbd1983099a8ba2c77ceb53aa0fe7e89eaebde5
“baozhigang” <646637969@qq.com>
2021-10-24 11:58:27 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Array/leetCode-283-moveZeroes.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	vals := []int{}
+	for ; head != nil; head = head.Next {
+		vals = append(vals, head.Val)
+	}
+	n := len(vals)
+	for i, v := range vals[:n/2] {
+		if v != vals[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+
+
