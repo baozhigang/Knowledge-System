18b46e2a15b9ac8d36582d1e3997a073009b5274
“baozhigang” <646637969@qq.com>
2021-10-25 19:08:54 +0800

    Update

---
+++ Data-Structures-And-Algorithms/BinarySearch/leetCode-287-findDuplicate.go
@@ -0,0 +1,20 @@
+func findDuplicate(nums []int) int {
+	left, right := 1, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		cnt := 0
+		for _, v := range nums {
+			if v <= mid {
+				cnt++
+			}
+		}
+
+		if cnt > mid {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
