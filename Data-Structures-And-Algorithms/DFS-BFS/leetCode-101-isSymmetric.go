10646871456edb80ac2834ac5b95be988e4dacc7
“baozhigang” <646637969@qq.com>
2021-10-27 11:11:34 +0800

    Update

---
+++ Data-Structures-And-Algorithms/DFS-BFS/leetCode-101-isSymmetric.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return dfs(root.Left, root.Right)
+}
+func dfs(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return dfs(left.Left, right.Right) && dfs(left.Right, right.Left)
+}
+
+
