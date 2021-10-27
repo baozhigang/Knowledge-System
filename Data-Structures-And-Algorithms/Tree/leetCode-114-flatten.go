316bf26dbe0f690bcaaa6cb4e8ba1a37cebc6c00
“baozhigang” <646637969@qq.com>
2021-10-27 17:53:08 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Tree/leetCode-114-flatten.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode) {
+	curr := root
+	for curr != nil {
+		if curr.Left != nil {
+			next := curr.Left
+			predecessor := next
+			for predecessor.Right != nil {
+				predecessor = predecessor.Right
+			}
+			predecessor.Right = curr.Right
+			curr.Left = nil
+			curr.Right = next
+		}
+		curr = curr.Right
+	}
+}
+
+
