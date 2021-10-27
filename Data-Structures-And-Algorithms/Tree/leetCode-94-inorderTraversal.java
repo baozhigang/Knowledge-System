147d0e05e9d90dd9600a8d9f4f05ed204ac30a74
“baozhigang” <646637969@qq.com>
2021-10-27 16:26:38 +0800

    Update

---
+++ Data-Structures-And-Algorithms/Tree/leetCode-94-inorderTraversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        inOrder(root, list);
+        return list;
+    }
+    public void inOrder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+    }
+}
