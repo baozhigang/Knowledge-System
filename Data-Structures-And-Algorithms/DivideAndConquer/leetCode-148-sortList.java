c3d076fa105d2d68c4220c7dc3546f3a57618ed1
“baozhigang” <646637969@qq.com>
2021-11-01 13:01:06 +0800

    Update

---
+++ Data-Structures-And-Algorithms/DivideAndConquer/leetCode-148-sortList.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode p2 = sortList(slow.next);
+        slow.next = null;
+        ListNode p1 = sortList(head);
+        return merge(p1, p2);
+    }
+
+    private ListNode merge(ListNode p1, ListNode p2) {
+        ListNode p, dummy = new ListNode(0);
+        p = dummy;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p.next = p1 == null ? p2 : p1;
+
+        return dummy.next;
+    }
+}
