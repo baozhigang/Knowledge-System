b69b5930304eb0cafdfc92bf8bc694368656a50c
“baozhigang” <646637969@qq.com>
2021-12-12 12:02:58 +0800

    Update

---
+++ GO/Practice/2021/12/switch1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var v1 int = 1
+	var v2 int64 = 234
+	var v3 string = "hello"
+	var v4 float32 = 1.23
+
+	MyPrintf(v1, v2, v3, v4)
+}
+
+func MyPrintf(args ...interface{}) {
+	for _, arg := range args {
+		switch arg.(type) {
+		case int:
+			fmt.Println(arg, "is an int value")
+		case string:
+			fmt.Println(arg, "is an string value")
+		case int64:
+			fmt.Println(arg, "is an int64 value")
+		default:
+			fmt.Println(arg, "is an unkown type")
+		}
+	}
+}
